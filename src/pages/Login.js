632fa2f7431515243fe012416d68c4c2766e5cf0
JC Dourado <jairocdourado@gmail.com>
2023-03-17 16:20:40 -0300

    iniciando o projeto Trivia

---
+++ src/pages/Login.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class Login extends Component {
+  render() {
+    return (
+      <div>
+        <section>
+          name
+        </section>
+      </div>
+    );
+  }
+}
+
+export default Login;
